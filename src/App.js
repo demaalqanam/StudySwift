f4336c22799c9e1ddf8611e1975b4b38074af598
demaalqanam <demaalqanam@gmail.com>
2024-01-30 19:15:46 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './Style/App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="btn btn-warning p-5">Hi World\\</h1>
+    </div>
+  );
+}
+
+export default App;
